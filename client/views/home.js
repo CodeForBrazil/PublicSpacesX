587c39c0305a1d5e14ef21ae5b65570fc07b594f
Neri Junior <neri@nerijunior.com>
2016-03-08 15:16:32 -0300

    fix: Formulario da home - veja home.js Template.home.onDestroyed

--- client/views/home.js
+++ client/views/home.js
@@ -3,8 +3,26 @@ Template.home.rendered = function() {
     $(window).scroll(function() {
         if( $(window).scrollTop() > 150) {
             $('.navbar').addClass('solid');
-        } else { 
+        } else {
         	$('.navbar').removeClass('solid');
         }
-    });    
+    });
+
+    // Form widget
+    ! function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0],
+            p = /^http:/.test(d.location) ? "http" : "https";
+
+        if (!d.getElementById(id)) {
+            js = d.createElement(s);
+            js.id = id;
+            js.src = p + "://s3.amazonaws.com/subscription-cdn/0.2/widget.min.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }
+    }(document, "script", "sendgrid-subscription-widget-js");
 };
+
+Template.home.onDestroyed(function(){
+    var formWidgetElement = document.getElementById('sendgrid-subscription-widget-js');
+    formWidgetElement.parentNode.removeChild(formWidgetElement);
+});
